53e857e6486849c3327416e5662ae77e74a9cfbe
Miguel Perez <104270468+M1guelPerez01@users.noreply.github.com>
2023-03-13 17:47:28 -0700

    update

--- cheatsheet.py
+++ cheatsheet.py
@@ -9,4 +9,21 @@ y = "de"
 z = X + y
 z
 
-z = 
+z = z [1:3]
+z
+
+#Functions
+def f(X):
+    y = x+2
+    return y
+f(5)
+
+#If Statements
+x = 2
+if x+1 == 3:
+    print (x)
+
+#Loops
+for i in range(1,100):
+    if i%2 == 0:
+        print(i)
